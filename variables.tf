022aec2da9e69e86723ea8948068a88fbe1a71dd
patilpankaj20 <patilpankaj20@outlook.com>
2025-09-17 07:06:52 +0000

    Add workspace tag and make variables required

--- variables.tf
+++ variables.tf
@@ -11,21 +11,16 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
-  default     = "globo-dev"
 }
 
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
-  default     = "10.42.0.0/16"
 }
 
 variable "public_subnets" {
   type        = map(string)
   description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
-  default = {
-    public-1 = "10.42.10.0/24"
-    public-2 = "10.42.11.0/24"
   }
 }
 variable "billing_code" {
@@ -35,5 +30,4 @@ variable "billing_code" {
 variable "environment" {
   type        = string
   description = "(Optional) Environemt for all resources"
-  default     = "development"
 }
